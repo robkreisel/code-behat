bda554a40c4eff32068ce26eb81bb72475d5e228
Rob Kreisel <rob@design1010.com>
2018-11-13 11:40:15 -0600

    Lesson 10

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -4,16 +4,15 @@ use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
-use Behat\MinkExtension\Context\MinkContext;
+use Behat\MinkExtension\Context\RawMinkContext;
 
 require_once __DIR__.'/../../vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
 
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext implements Context, SnippetAcceptingContext
+class FeatureContext extends RawMinkContext implements Context, SnippetAcceptingContext
 {
-
     private $output;
 
     /**
@@ -85,4 +84,30 @@ class FeatureContext implements Context, SnippetAcceptingContext
         mkdir($dir);
     }
 
+    /**
+     * @When I fill in the search box with :term
+     */
+    public function iFillInTheSearchBoxWith($term)
+    {
+        $searchBox = $this->assertSession()
+            ->elementExists('css', 'input[name="searchTerm"]');
+
+        $searchBox->setValue($term);
+    }
+
+    /**
+     * @When I press the search button
+     */
+    public function iPressTheSearchButton()
+    {
+        $button = $this->assertSession()
+            ->elementExists('css', '#search_submit');
+
+        $button->press();
+    }
+
+    private function getPage()
+    {
+        return $this->getSession()->getPage();
+    }
 }
